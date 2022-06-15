fd5c149c3f601b886af0a03044a1f16ab94df341
Karan Thakkar <karan.thakkar97@gmail.com>
2022-06-15 14:55:40 -0500

    Update app.py

--- code/app.py
+++ code/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # Connecting to database
 
-host = "tcp:cse6331.database.windows.net"
-db = "cse6331assignment2"
-user = "kjtcse6331"
-pwd ="AzureSQL@CSE6331"
+host = ""
+db = ""
+user = ""
+pwd =""
 driver= '{SQL Server}'
 # driver= '{ODBC Driver 17 for SQL Server}'
 connection = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+host+';PORT=1443;DATABASE='+db+';UID='+user+';PWD='+ pwd)
